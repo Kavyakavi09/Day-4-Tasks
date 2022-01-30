73f30d9a462c169401d5261473a00dcf6a1a6437
Kavyakavi09 <95955787+Kavyakavi09@users.noreply.github.com>
2022-01-30 08:13:09 -0600

    Create XMLflag.js

---
+++ XMLflag.js
@@ -0,0 +1,19 @@
+var request = new XMLHttpRequest();
+
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+  
+request.send();
+
+request.onload =function(){
+    if(request.status>=200 && request.status<300){
+    var data = JSON.parse(request.response);
+   \\ console.log(data);
+   for(var i=0;i<data.length;i++){
+        console.log(`Country:${data[i].name}
+Flag:${data[i].flag}`);
+    }
+}
+else{
+    console.log("Data is not available");
+}
+};
